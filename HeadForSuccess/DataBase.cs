18d288cb94c6d293be95c95ca3e2559501cf21c9
bparrishMines <bparrish@mines.edu>
2015-05-20 15:41:16 -0700

    Changing Database to save and load data base

--- HeadForSuccess/DataBase.cs
+++ HeadForSuccess/DataBase.cs
@@ -4,61 +4,108 @@ using System.Linq;
 using System.Text;
 using Foundation;
 using UIKit; 
-using System.Data.SQLite; 
-//using SQLite;
+using System.Data.SQLite;
+using System.Data;
 
 namespace HeadForSuccess
-{  
-    
+{
+
     class DataBase
     {
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            DataBase dB = new DataBase();
+            Console.ReadLine();
 
-        SQLiteConnection dataBase;  
+        }
+
+        static String DB_PATH = "/Data/";
+        static String DB_NAME = "DataBase.sqlite";
+
+        SQLiteConnection dataBase;
 
         DataBase()
         {
             createNewDataBase();
-            connectToDataBase();
-        } 
-
-        private void createNewDataBase()
-        {
-            SQLiteConnection.CreateFile("DataBase.sqlite"); 
+            //connectToDataBase();
+            //createTable();
+            fillTable();
+            printNames();
         }
 
-        private void connectToDataBase()
+        void createNewDataBase()
         {
-            dataBase = new SQLiteConnection("Data Source=DataBase.sqlite;Version=3;");
-            dataBase.Open();
-        }   
-         
-        ////////////////////////////////////////////////////////////////TESTING METHODS//////////////////////////////////////////////////////
+            dataBase = new SQLiteConnection("Data Source=" + DB_NAME + ";Version=3;");
+            Boolean dbExist = existDataBase();
 
-        // For Testing
-        private void createTable()
+            if (!dbExist)
+            {
+                SQLiteConnection.CreateFile(DB_NAME);
+                dataBase = new SQLiteConnection("Data Source=" + DB_NAME + ";Version=3;");
+                dataBase.Open();
+                createTable();
+            }
+            else
+            {
+                dataBase = new SQLiteConnection("Data Source=" + DB_NAME + ";Version=3;");
+                dataBase.Open();
+            }
+
+
+        }
+
+        public bool existDataBase()
+        {
+            Boolean checkDB = true;
+            using (var connection = new SQLiteConnection(dataBase.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception)
+                {
+                    checkDB = false;
+                }
+            }
+
+            return checkDB;
+        }
+
+        //void connectToDataBase()
+        //{
+        //    dataBase = new SQLiteConnection("Data Source=DataBase.sqlite;Version=3;");
+        //    dataBase.Open();
+        //} 
+
+        void createTable()
         {
             string sql = "create table clients (name varechar(20))";
             SQLiteCommand command = new SQLiteCommand(sql, dataBase);
-            command.ExecuteNonQuery();  
-        } 
-         
-        // For Testing
-        private void fillTable() 
+            command.ExecuteNonQuery();
+        }
+
+        void fillTable()
         {
-            string sql = "insert into clients (name) values ('Bob')";
+            string sql = "insert into clients (name) values ('h')";
             SQLiteCommand command = new SQLiteCommand(sql, dataBase);
             command.ExecuteNonQuery();
-        } 
-         
-        // For Testing
-        private void printNames()
+            sql = "insert into clients (name) values ('t')";
+            command = new SQLiteCommand(sql, dataBase);
+            command.ExecuteNonQuery();
+        }
+
+        void printNames()
         {
-            string sql = "select * from clients order by name desc";
+            string sql = "select * from clients order by name asc";
             SQLiteCommand command = new SQLiteCommand(sql, dataBase);
             SQLiteDataReader reader = command.ExecuteReader();
             while (reader.Read())
                 Console.WriteLine("Name: " + reader["name"]);
             Console.ReadLine();
         }
+
     }
 }
